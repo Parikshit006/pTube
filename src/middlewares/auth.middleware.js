dc1f0e4d84156745260bfc3a2a192d783148e896
Parikshit006 <parikshitpatil006@gmail.com>
2026-02-07 22:38:47 +0530

    access and refresh tokens

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,5 +1,5 @@
-import { ApiError } from "../utils/ApiError";
-import { asyncHandler } from "../utils/asyncHandler";
+import { ApiError } from "../utils/ApiError.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
 import jwt from "jsonwebtoken";
 import { User } from "../models/user.model.js";
 
@@ -9,6 +9,7 @@ export const verifyJWT = asyncHandler(async (req, _, next) => {
       req.cookies?.accessToken ||
       req.header("Authorization")?.replace("Bearer ", "");
 
+    // console.log(token);
     if (!token) {
       throw new ApiError(401, "Unauthorized request");
     }
@@ -20,12 +21,12 @@ export const verifyJWT = asyncHandler(async (req, _, next) => {
     );
 
     if (!user) {
-      throw new ApiError(402, "invalid Access token ");
+      throw new ApiError(401, "Invalid Access Token");
     }
 
     req.user = user;
     next();
   } catch (error) {
-    throw new ApiError(401,error?.message || "invalid access token");
+    throw new ApiError(401, error?.message || "Invalid access token");
   }
 });
