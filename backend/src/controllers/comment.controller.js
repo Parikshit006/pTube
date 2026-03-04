9c19cfdaa3a5e47c0d68a368627c8e390545a596
Parikshit006 <parikshitpatil006@gmail.com>
2026-03-04 19:42:43 +0530

    Enhance comment and subscription controllers with validation and aggregation features

--- backend/src/controllers/comment.controller.js
+++ backend/src/controllers/comment.controller.js
@@ -8,19 +8,139 @@ const getVideoComments = asyncHandler(async (req, res) => {
     //TODO: get all comments for a video
     const {videoId} = req.params
     const {page = 1, limit = 10} = req.query
+     if (!mongoose.isValidObjectId(videoId)) {
+       throw new ApiError(400, "Invalid video ID");
+     }
+
+     const aggregate = Comment.aggregate([
+       {
+         $match: {
+           video: new mongoose.Types.ObjectId(videoId),
+         },
+       },
+       {
+         $lookup: {
+           from: "users",
+           localField: "owner",
+           foreignField: "_id",
+           as: "owner",
+           pipeline: [
+             {
+               $project: {
+                 fullName: 1,
+                 username: 1,
+                 avatar: 1,
+               },
+             },
+           ],
+         },
+       },
+       {
+         $addFields: {
+           owner: { $first: "$owner" },
+         },
+       },
+       {
+         $sort: { createdAt: -1 },
+       },
+     ]);
+
+     const options = {
+       page: parseInt(page),
+       limit: parseInt(limit),
+     };
+
+     const comments = await Comment.aggregatePaginate(aggregate, options);
+
+     return res
+       .status(200)
+       .json(
+         new ApiResponse(200, comments, "Video comments fetched successfully")
+       );
 
 })
 
 const addComment = asyncHandler(async (req, res) => {
     // TODO: add a comment to a video
+     const { videoId } = req.params;
+     const { content } = req.body;
+
+     if (!mongoose.isValidObjectId(videoId)) {
+       throw new ApiError(400, "Invalid video ID");
+     }
+
+     if (!content || content.trim() === "") {
+       throw new ApiError(400, "Comment content is required");
+     }
+
+     const comment = await Comment.create({
+       content,
+       video: videoId,
+       owner: req.user._id,
+     });
+
+     return res
+       .status(201)
+       .json(new ApiResponse(201, comment, "Comment added successfully"));
 })
 
 const updateComment = asyncHandler(async (req, res) => {
     // TODO: update a comment
+      const { commentId } = req.params;
+      const { content } = req.body;
+
+      if (!mongoose.isValidObjectId(commentId)) {
+        throw new ApiError(400, "Invalid comment ID");
+      }
+
+      if (!content || content.trim() === "") {
+        throw new ApiError(400, "Comment content cannot be empty");
+      }
+
+      const comment = await Comment.findById(commentId);
+
+      if (!comment) {
+        throw new ApiError(404, "Comment not found");
+      }
+
+      if (comment.owner.toString() !== req.user._id.toString()) {
+        throw new ApiError(
+          403,
+          "You are not authorized to update this comment"
+        );
+      }
+
+      comment.content = content;
+      await comment.save({ validateBeforeSave: false });
+
+      return res
+        .status(200)
+        .json(new ApiResponse(200, comment, "Comment updated successfully"));
 })
 
 const deleteComment = asyncHandler(async (req, res) => {
     // TODO: delete a comment
+     const { commentId } = req.params;
+
+     if (!mongoose.isValidObjectId(commentId)) {
+       throw new ApiError(400, "Invalid comment ID");
+     }
+
+     const comment = await Comment.findById(commentId);
+
+     if (!comment) {
+       throw new ApiError(404, "Comment not found");
+     }
+
+     if (comment.owner.toString() !== req.user._id.toString()) {
+       throw new ApiError(403, "You are not authorized to delete this comment");
+     }
+
+     await comment.deleteOne();
+
+     return res
+       .status(200)
+       .json(new ApiResponse(200, {}, "Comment deleted successfully"));
 })
 
 export {
--- backend/src/controllers/subscription.controller.js
+++ backend/src/controllers/subscription.controller.js
@@ -9,16 +9,113 @@ import {asyncHandler} from "../utils/asyncHandler.js"
 const toggleSubscription = asyncHandler(async (req, res) => {
     const {channelId} = req.params
     // TODO: toggle subscription
+
+     if (!isValidObjectId(channelId)) {
+       throw new ApiError(400, "Invalid channel ID");
+     }
+
+     const existingSubscription = await Subscription.findOne({
+       subscriber: req.user._id,
+       channel: channelId,
+     });
+
+     if (existingSubscription) {
+       await existingSubscription.deleteOne();
+
+       return res
+         .status(200)
+         .json(new ApiResponse(200, {}, "Unsubscribed successfully"));
+     }
+
+     const subscription = await Subscription.create({
+       subscriber: req.user._id,
+       channel: channelId,
+     });
+
+     return res
+       .status(201)
+       .json(new ApiResponse(201, subscription, "Subscribed successfully"));
 })
 
 // controller to return subscriber list of a channel
 const getUserChannelSubscribers = asyncHandler(async (req, res) => {
-    const {channelId} = req.params
+    const {channelId} = req.params;
+
+     if (!isValidObjectId(channelId)) {
+       throw new ApiError(400, "Invalid channel ID");
+     }
+
+     const existingSubscription = await Subscription.findOne({
+       subscriber: req.user._id,
+       channel: channelId,
+     });
+
+     if (existingSubscription) {
+       await existingSubscription.deleteOne();
+
+       return res
+         .status(200)
+         .json(new ApiResponse(200, {}, "Unsubscribed successfully"));
+     }
+
+     const subscription = await Subscription.create({
+       subscriber: req.user._id,
+       channel: channelId,
+     });
+
+     return res
+       .status(201)
+       .json(new ApiResponse(201, subscription, "Subscribed successfully"));
 })
 
 // controller to return channel list to which user has subscribed
 const getSubscribedChannels = asyncHandler(async (req, res) => {
-    const { subscriberId } = req.params
+    const { subscriberId } = req.params;
+
+        if (!isValidObjectId(subscriberId)) {
+          throw new ApiError(400, "Invalid subscriber ID");
+        }
+
+        const subscribedChannels = await Subscription.aggregate([
+          {
+            $match: {
+              subscriber: new mongoose.Types.ObjectId(subscriberId),
+            },
+          },
+          {
+            $lookup: {
+              from: "users",
+              localField: "channel",
+              foreignField: "_id",
+              as: "channel",
+              pipeline: [
+                {
+                  $project: {
+                    fullName: 1,
+                    username: 1,
+                    avatar: 1,
+                  },
+                },
+              ],
+            },
+          },
+          {
+            $addFields: {
+              channel: { $first: "$channel" },
+            },
+          },
+        ]);
+
+        return res.status(200).json(
+          new ApiResponse(
+            200,
+            {
+              totalSubscribedChannels: subscribedChannels.length,
+              subscribedChannels,
+            },
+            "Subscribed channels fetched successfully"
+          )
+        );
 })
 
 export {
