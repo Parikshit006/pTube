245c50c795ea469b69acb1612b39c90502501a55
Parikshit006 <parikshitpatil006@gmail.com>
2026-03-15 19:42:14 +0530

    dashboard controller

--- backend/src/controllers/dashboard.controller.js
+++ backend/src/controllers/dashboard.controller.js
@@ -8,10 +8,77 @@ import {asyncHandler} from "../utils/asyncHandler.js"
 
 const getChannelStats = asyncHandler(async (req, res) => {
     // TODO: Get the channel stats like total video views, total subscribers, total videos, total likes etc.
+      const channelId = req.user._id;
+
+      const totalVideos = await Video.countDocuments({
+        owner: channelId,
+      });
+
+      const totalViews = await Video.aggregate([
+        {
+          $match: { owner: channelId },
+        },
+        {
+          $group: {
+            _id: null,
+            totalViews: { $sum: "$views" },
+          },
+        },
+      ]);
+
+      const totalSubscribers = await Subscription.countDocuments({
+        channel: channelId,
+      });
+
+      const totalLikes = await Like.aggregate([
+        {
+          $lookup: {
+            from: "videos",
+            localField: "video",
+            foreignField: "_id",
+            as: "video",
+          },
+        },
+        {
+          $unwind: "$video",
+        },
+        {
+          $match: {
+            "video.owner": new mongoose.Types.ObjectId(channelId),
+          },
+        },
+        {
+          $count: "totalLikes",
+        },
+      ]);
+
+      return res.status(200).json(
+        new ApiResponse(
+          200,
+          {
+            totalVideos,
+            totalViews: totalViews[0]?.totalViews || 0,
+            totalSubscribers,
+            totalLikes: totalLikes[0]?.totalLikes || 0,
+          },
+          "Channel stats fetched successfully"
+        )
+      );
 })
 
 const getChannelVideos = asyncHandler(async (req, res) => {
     // TODO: Get all the videos uploaded by the channel
+      const channelId = req.user._id;
+
+      const videos = await Video.find({
+        owner: channelId,
+      }).sort({ createdAt: -1 });
+
+      return res
+        .status(200)
+        .json(
+          new ApiResponse(200, videos, "Channel videos fetched successfully")
+        );
 })
 
 export {
